4fb43a8737f0aefa0fa520f335332ddf8e319cd9
Alister A. Fenix <a.fenix@icloud.com>
2015-02-20 12:26:37 -0800

    Add Pizza object slices method

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-var Pizza = { size: [10, 12, 18],
-              flavor: ["cheese", "pepperoni"],
+var Pizza = { size: 0,
+              flavor: "",
               slices: function() {
-                numberSlice = this.size - 4;
+                var numberSlice = this.size - 4;
                 return numberSlice;
               }
 };
@@ -10,4 +10,15 @@ $(document).ready(function() {
   $("form#pizza").submit(function(event) {
    event.preventDefault();
 
+   var inputtedSize = parseInt($("input#size").val());
+   var inputtedFlavor = $("input#flavor").val();
+   var newPizza = Object.create(Pizza);
+   newPizza.size = inputtedSize;
+   newPizza.flavor = inputtedFlavor;
+   newPizza.slices(inputtedSize);
+
+   $("input#size").val("");
+   $("input#flavor").val("");
+
+  });
 });
--- specs/specs.js
+++ specs/specs.js
@@ -1,9 +1,9 @@
 describe("Pizza", function() {
   describe("slices", function() {
-    it("calculates how many slices of pizza based off the given perimeter", function () {
+    it("calculates how many slices of pizza based off the given perimeter", function() {
       var testSlice = Object.create(Pizza);
-      testSlice.size(12);
-      expect(testSlice.slices).to.equal(8);
+      testSlice.size = 12;
+      expect(testSlice.slices()).to.equal(8);
     });
   });
 });
