3b6b4ce838539e2f5f2a4c5a7d6821b968bb82ca
Alister A. Fenix <a.fenix@icloud.com>
2015-02-20 15:13:22 -0800

    Add fancy font styling to the output of the results

--- specs/specs.js
+++ specs/specs.js
@@ -2,7 +2,7 @@ describe("Pizza", function() {
   describe("slices", function() {
     it("calculates how many slices of pizza based off the given perimeter and style", function() {
       var testSlice = Object.create(Pizza);
-      testSlice.style = "Deep dish"
+      testSlice.style = "Deep Dish"
       testSlice.size = 12;
       expect(testSlice.slices()).to.equal(6);
     });
